a56f66a6c0558a3b5889c5e708a11b01dc70e4ec
Danny Lee <dreamer@panix.com>
2019-10-29 22:11:20 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function wakeDog(name, breed) {
+    console.log(`Wake ${name} the ${breed}`);
+    return `Wake ${name} the ${breed}`;
+}
+
+function leashDog(name, breed) {
+    console.log(`Leash ${name} the ${breed}`);
+    return `Leash ${name} the ${breed}`;
+}
+
+function walkToPark(name, breed) {
+    console.log(`Walk to the park with ${name} the ${breed}`);
+    return `Walk to the park with ${name} the ${breed}`;
+}
+
+function throwFrisbee(name, breed) {
+    console.log(`Throw the frisbee for ${name} the ${breed}`);
+    return `Throw the frisbee for ${name} the ${breed}`;
+}
+
+function walkHome(name, breed) {
+    console.log(`Walk home with ${name} the ${breed}`);
+    return `Walk home with ${name} the ${breed}`;
+}
+
+function unleashDog(name, breed) {
+    console.log(`Unleash ${name} the ${breed}`);
+    return `Unleash ${name} the ${breed}`;
+}
+
+const routine = [ wakeDog, leashDog, walkToPark, throwFrisbee,
+    walkHome, unleashDog ];
+
+function exerciseDog(name, breed) {
+    const return_array = [];
+    routine.forEach(function(routine_function) {
+        return_array.push(routine_function(name, breed));
+    });
+    return return_array;
+}
